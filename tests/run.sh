87b55c92aa7b07b76b3785800601600323d0c9a7
rashed <rashed.sarwar@ukaea.uk>
2024-11-01 20:21:10 +0000

    add DISPLAY to run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it --privileged -v ${PWD}:/home/containeruser/wkspace:Z -u ${UID}:${GID}  -w /home/containeruser/wkspace cocotb_with_ghdl make
+docker run --rm -it --privileged -v ${PWD}:/home/containeruser/wkspace:Z -u ${UID}:${GID} -e DISPLAY=$DISPLAY \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix -w /home/containeruser/wkspace cocotb_with_ghdl make
